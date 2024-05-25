613a9f9ec6bca078d39f1364883548e866e46cfe
Ramirez-Z <155502769+Ramirez-Z@users.noreply.github.com>
2024-05-25 11:51:11 +0800

    Add files via upload

---
+++ Practice.py
@@ -0,0 +1,20 @@
+# fighters = tuple(("Mustang", "Lightning", "Thunderbolt"))
+# bombers = tuple(("Catalina", "Flying Fortress", "Mitchell"))
+# x = list(bombers)
+# x.append("Superfortress")
+# y = tuple(x)
+
+# print(fighters)
+# print()
+# print(bombers)
+# print()
+# print(y)
+# print()
+
+# from tkinter import *
+# root = Tk()
+
+# my_label = Label(root, text = "Hello World!")
+# my_label.pack()
+
+# root.mainloop()
